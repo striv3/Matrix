2aa20f47307a27d04b37f8bf21dc6fa852e3ae06
striv3 <39053087+striv3@users.noreply.github.com>
2018-05-26 00:16:58 +0530

    Update Matrix_Class.hpp

--- Matrix_Class.hpp
+++ Matrix_Class.hpp
@@ -1,1 +1,19 @@
+ //header File For Matrix Class
+#ifndef matrix
+#define matrix
+#include<vector>
+class matrix{
+public:
+     matrix()=default;
+     matrix(const std::vector<const std::vector<int> >);
+     matrix(unsigned,unsigned,const std::vector<const std::vector<int> >);
+    friend std::istream& read(std::istream&,matrix);
+    friend std::ostream& print(std::ostream&,const matrix);
+    friend matrix& adj( matrix);
+    friend matrix& inverse( matrix);
 
+private:
+    unsigned rows=0,columns=0;
+    std::vector<std::vector<int> > elements;
+};
+#endif // Matrix
