2b3a716a3d0572125099df454f4e45b0768e7b53
striv3 <39053087+striv3@users.noreply.github.com>
2018-05-27 05:56:17 +0530

    Update Matrix_Class.cpp
    Added Copy constructor

--- Matrix_Class.cpp
+++ Matrix_Class.cpp
@@ -1,27 +1,9 @@
-#include"matrix_Class"
+#include"matrix_Class1.hpp"
 #include<vector>
 
+matrix::matrix():rows(0),columns(0),elements(vector<vector<int>(0) >(0));{}
 
-/*#ifndef matrix
-#define matrix
-#include<vector>
-class matrix{
-public:
-     matrix()=default;
-     matrix(const std::vector<std::vector<int> >);
-     matrix(rows,columns,const std::vector<std::vector<int> >);
-    friend std::istream& read(std::istream&,matrix);
-    friend std::ostream& print(std::ostream&,const matrix);
-    friend matrix& adj(const matrix);
-    friend matrix& inverse(const matrix);
-
-private:
-    unsigned rows=0,columns=0;
-    std::vector<std::vector<int> > elements;
-};
-#endif // Matrix
-*/
-matrix(const  std::vector<const std::vector<int> > &v)::rows(v.size()),columns(v[0].size())//Cannot do Direct Initialization
+matrix::matrix(const  std::vector< std::vector<int> > &v):rows(v.size()),columns(v[0].size())//Cannot do Direct Initialization
 {
     for(auto i=0;i!=rows;++i)
         elements.push_back(vector<int>());
@@ -29,15 +11,17 @@ matrix(const  std::vector<const std::vector<int> > &v)::rows(v.size()),columns(v
         for(auto j=0;j!=columns;++j)
             elements[i].push_back(v[i][j]);
 }
-matrix(unsigned r,unsigned c,const std::vector<const std::vector<int> > &v)::rows(r),columns(c)
+matrix::matrix(unsigned r,unsigned c,const std::vector<const std::vector<int> > &v):rows(r),columns(c)
 {
      for(auto i=0;i!=rows;++i)
         elements.push_back(vector<int>());
     for(auto i=0;i!=rows;++i)
         for(auto j=0;j!=columns;++j)
             elements[i].push_back(v[i][j]);
+
 }
-std::istream& read(std::istream& is,matrix &A)
+matrix::matrix(const matrix&obj):rows(obj.rows),columns(obj.columns),elements(obj.elements);
+matrix::istream& read(istream& is,matrix &A)
 {
     is>>A.rows>>A.columns;
     for(auto i=0;i!=A.rows;++i)
@@ -50,7 +34,7 @@ std::istream& read(std::istream& is,matrix &A)
             }
 return is;
 }
-std::ostream& print(std::ostream &os,matrix &A)
+matrix::ostream& print(ostream &os,matrix &A)
 {
     for(auto i=0;i!=A.rows;++i)
         {
@@ -60,3 +44,4 @@ std::ostream& print(std::ostream &os,matrix &A)
         os>>"\n";
         }
 }
+
